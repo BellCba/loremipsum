ef0d2646e836cc4c64b2eb493df23073a8abf483
BellCba <bell.933.cba@gmail.com>
2023-04-18 20:29:54 -0300

    commit necesario

--- lipsum.sh
+++ lipsum.sh
@@ -3,7 +3,7 @@
 for i in {1..5}
 do
     #obtiene una cantidad aleatoria de número de lineas entre 1 y 20
-    lineas=$(shuf -i 1-20 -n 1)
+    lineas=$(shuf -i 1-20 -n 1)bash
     #llama con curl a lipsum.com y le pasa como parámetro la cantidad de líneas
     lorem=$(curl -s https://www.lipsum.com/feed/xml\?amount\=$lineas | sed -n '/<lipsum>/,/<\/lipsum>/p' | sed -e 's/<lipsum>//g' -e 's/<\/lipsum>//g'
 )   
